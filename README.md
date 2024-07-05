85df7a7c4736b9a8248d3a928e8face83c96bb45
Xie_Qiang <Xie_Qiang@turn2cloud.com>
2024-07-05 17:13:44 +0800

    2024/07/05

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 ### [搭建 Vite 項目](https://cn.vitejs.dev/guide/)
 
 ```ps
+# 安裝 Node.js
+node -v
+npm -v
+#
 npm create vite@latest
 # .\vue3_test\index.html (Vite 項目的入口文件)
 # 進入工程根目錄
@@ -19,8 +23,9 @@ npm install -g typescript
 
 ```ps
 # 插件，可在 <script setup lang="ts"> 中定義組件名稱 <script setup lang="ts" name="***">
+# -D, --save-dev: Package will appear in your devDependencies.
 npm install vite-plugin-vue-setup-extend -D
 
 # ref 基本類型數據的響應式變量
-# reactive 對象類型數據的響應式變量
+# reactive 只能定義對象類型數據的響應式變量；重新分配對象之後，會失去響應式(需要使用 Object.assign(car, { brand: '奧拓', price: 6 }) 重新分派)
 ```
--- code/vue3_test/src/components/Person.vue
+++ code/vue3_test/src/components/Person.vue
@@ -2,43 +2,28 @@
     <div class="person">
         <h2>姓名：{{ name }}</h2>
         <h2>年齡：{{ age }}</h2>
-        <hr>
         <button @click="changeName">修改姓名</button>
         <button @click="changeAge">修改年齡</button>
         <button @click="showTel">查看聯係方式 </button>
+        <hr>
+        <h2>一輛越野車：{{ car.brand }}，價格：{{ car.price }} 萬圓</h2>
+        <button @click="changeCarPrice">修改越野車價格</button>
+        <hr>
+        <ul>
+            <li v-for="game in games" :key="game.id">{{ game.name }}</li>
+        </ul>
+        <button @click="changeFirstGameName">修改第一個游戲的名稱</button>
+        <hr>
+        <h2>一輛越野車：{{ car1.brand }}，價格：{{ car1.price }} 萬圓</h2>
+        <button @click="changeCarPrice">修改越野車價格</button>
     </div>
 </template>
 
-<script lang="ts">
-export default {
-    name: 'Person',
-    // setup() {
-    //     // 數據
-    //     let name = '瘦毛毛' // 此時變量非響應式
-    //     let age = 33 // 此時變量非響應式
-    //     let tel = '13888888888' // 此時變量非響應式
-
-    //     // 方法
-    //     function changeName() {
-    //         name = 'M_0v0_M' // 如此修改變量，頁面沒有變化
-    //     }
-    //     function changeAge() {
-    //         age += 1
-    //     }
-    //     function showTel() {
-    //         alert(tel)
-    //     }
-
-    //     // 暴露
-    //     return { name, age, changeName, changeAge, showTel }
-    //     // setup 的返回值也可以為一個渲染函數
-    //     // return () => { '哈哈' }
-    // }
-}
-</script>
 <!--setup 語法糖 自動返回暴露-->
-<script setup lang="ts">
-import { ref } from 'vue';
+<!--通過安裝插件 npm install vite-plugin-vue-setup-extend -D-->
+<!--無需再書寫 export default -> name:"Person"-->
+<script setup lang="ts" name="Person">
+import { ref, reactive } from 'vue';
 
 // 數據
 // let name = '瘦毛毛' // 此時變量非響應式
@@ -46,6 +31,20 @@ let name = ref('瘦毛毛') // ref 基本類型的響應式變量
 // let age = 11 // 此時變量非響應式
 let age = ref(11) // ref 基本類型的響應式變量
 let tel = '13888888888' // 此時變量非響應
+//
+let car = reactive({ brand: '長城-TANK300', price: 33 })
+console.log(car)
+//
+let games = reactive([
+    { id: '00001', name: 'GAME_00001' },
+    { id: '00003', name: 'GAME_00002' },
+    { id: '00003', name: 'GAME_00003' }
+])
+console.log(games)
+//
+let car1 = ref({ brand: '長城-TANK300', price: 33 })
+console.log(car1)
+
 // 方法
 function changeName() {
     name.value = 'M_0v0_M' // 如此修改變量，頁面沒有變化
@@ -55,6 +54,19 @@ function changeAge() {
 }
 function showTel() {
     alert(tel)
+}
+/** 改變越野車價格 */
+function changeCarPrice() {
+    car.price += 1
+    //
+    car1.value.brand = '長城-TANK400'
+    car1.value.price += 1
+    //
+    // Object.assign(car, { brand: '奧拓', price: 6 })
+}
+/** 修改第一個游戲的名稱 */
+function changeFirstGameName() {
+    games[0].name = '魔獸爭霸III - 冰封王座'
 }
 </script>
 
